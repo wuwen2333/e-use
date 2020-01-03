9287cc562a5cf6d3d03d719ec3220952aa6ebddb
tangjd <tangjindi@shengzhidai.com>
2020-01-03 11:05:20 +0800

    encode and decode

--- src/conts/navigator.js
+++ src/conts/navigator.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Time from '../pages/Time'
+import Encode from '../pages/Encode'
 
 const navigator = [
   {
@@ -7,6 +8,13 @@ const navigator = [
     title: 'Time conversion',
     desc: 'Time conversion',
     component: <Time />,
+    icon: 'icon_time.png',
+  },
+  {
+    name: 'encode',
+    title: 'encode and decode',
+    desc: 'encode and decode',
+    component: <Encode />,
     icon: 'icon_time.png',
   },
 ]
