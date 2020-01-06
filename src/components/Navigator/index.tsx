187d2019ad707746385ea9430b41a0e3f3051255
Wen_Wu <Wen_Wu@epam.com>
2020-01-06 18:12:53 +0800

    add search box

--- src/components/Navigator/index.tsx
+++ src/components/Navigator/index.tsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import {
+  Form
+} from 'react-bootstrap'
 
 import './styles.scss'
 
@@ -10,13 +13,25 @@ interface Props {
 }
 
 const Navigator: React.FC<Props> = ({ currentPage, onPageChange }) => {
+  const [keyword, setKeyword] = React.useState('');
 
   return (
     <div className={`navigator ${currentPage && 'sidebar'}`}>
       <div className="nav-wrapper">
         <header>
-          <div className="logo" onClick={() => onPageChange()}></div>
-          <p>Ease to use</p>
+          <div className="logo" onClick={() => onPageChange()}>
+            <div className="icon"></div>
+            <p>Easy to use</p>
+          </div>
+          <div className="search">
+            <input
+              type="text"
+              placeholder="search..."
+              value={keyword}
+              onInput={(e:any) => setKeyword(e.target.value)}
+            />
+            {keyword && <div className="clear" title="clear" onClick={() => setKeyword('')}></div>}
+          </div>
         </header>
         <ul className="tool-list">
           {
